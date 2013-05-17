8c632574fcec4339cfdc9d30ba24f7e18a20eab1
Gabriel Lindstrom <gabriel.lindstrom@gmail.com>
2013-05-17 16:40:27 +0300

    added comments to Hand class

--- Poker/src/poker/Hand.java
+++ Poker/src/poker/Hand.java
@@ -4,16 +4,24 @@ package poker;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * A Hand object represents a five card poker hand.
+ * @see Card
+ */
 public class Hand 
 {
     private List<Card> cards;
-    
+        
     public Hand()
     {
         this.cards = new ArrayList();
     }
     
+    /**
+     * Adds a card to the Hand.
+     * @param c The Card to be added.
+     * @return Returns true if the card was successfully added, false otherwise.
+     */
     public boolean addCard(Card c)
     {
         if (this.cards.size() == 5)
