c67f05f3f874aad9a4a9bc7a71a77f93fc9c27ee
Gabriel Lindstrom <gabriel.lindstrom@gmail.com>
2013-05-24 11:53:08 +0300

    reverted back to older version of Rank as intToRank is redundant

--- Poker/src/poker/Rank.java
+++ Poker/src/poker/Rank.java
@@ -10,63 +10,61 @@ public enum Rank
     /**
      * Ordinal value 0. Abbreviation 'A'.
      */
-    ACE('A', 0),
+    ACE('A'),
     /**
      * Ordinal value 1. Abbreviation 'K'.
      */
-    KING('K', 1), 
+    KING('K'), 
     /**
      * Ordinal value 2. Abbreviation 'Q'.
      */
-    QUEEN('Q', 2), 
+    QUEEN('Q'), 
     /**
      * Ordinal value 3. Abbreviation 'J'.
      */
-    JACK('J', 3), 
+    JACK('J'), 
     /**
      * Ordinal value 4. Abbreviation 'T'.
      */
-    TEN('T', 4), 
+    TEN('T'), 
     /**
      * Ordinal value 5. Abbreviation '9'.
      */
-    NINE('9', 5),
+    NINE('9'),
     /**
      * Ordinal value 6. Abbreviation '8'.
      */
-    EIGHT('8', 6),
+    EIGHT('8'),
     /**
      * Ordinal value 7. Abbreviation '7'.
      */
-    SEVEN('7', 7),
+    SEVEN('7'),
     /**
      * Ordinal value 8. Abbreviation '6'.
      */
-    SIX('6', 8), 
+    SIX('6'), 
     /**
      * Ordinal value 9. Abbreviation '5'.
      */
-    FIVE('5', 9), 
+    FIVE('5'), 
     /**
      * Ordinal value 10. Abbreviation '4'.
      */
-    FOUR('4', 10),
+    FOUR('4'),
     /**
      * Ordinal value 11. Abbreviation '3'.
      */
-    THREE('3', 11), 
+    THREE('3'), 
     /**
      * Ordinal value 12. Abbreviation '2'.
      */
-    TWO('2', 12);    
+    TWO('2');    
     
     private char abbreviation;
-    private int value;
     
-    private Rank(char abbreviation, int value)
+    private Rank(char abbreviation)
     {
         this.abbreviation = abbreviation;
-        this.value = value;
     }
     
     /**
@@ -80,18 +78,6 @@ public enum Rank
         for (Rank rank : Rank.values())
         {
             if (rank.abbreviation == Character.toUpperCase(c))
-            {
-                return rank;
-            }
-        }
-        throw new IllegalArgumentException();
-    }
-    
-    public static Rank intToRank(int i)
-    {
-        for (Rank rank : Rank.values())
-        {
-            if (rank.value == i)
             {
                 return rank;
             }
