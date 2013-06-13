2557876226a5d2aaecbfc8505d5e7ec9e8e0db86
Gabriel Lindstrom <gabriel.lindstrom@gmail.com>
2013-06-13 15:49:13 +0300

    Edited comments in PokerCalculator

--- Poker/src/poker/calculator/PokerCalculator.java
+++ Poker/src/poker/calculator/PokerCalculator.java
@@ -9,22 +9,22 @@ public interface PokerCalculator
 {
     /**
      * Calculates the equity of two hands.
+     * Equity is defined as winning percentage + tie percentage / 2. For example,
+     * if the pot size is 100 and the equity is 60 %, it means that the expected
+     * stack size is 0.6*100=60.
      * @param hand1 string expression of the first hand
      * @param hand2 string expression of the second hand
      */
     void calculateEquity(String hand1, String hand2);
     
      /**
-     * Calculates the equity of the first hand.
-     * Equity is defined as winning percentage + tie percentage / 2. For example,
-     * if the pot size is 100 and the equity is 60 %, it means that the expected
-     * stack size is 0.6*100=60.
+     * Returns the equity of the first hand.
      * @return the equity expressed as a decimal number
      */
     double equity1();
     
     /**
-     * Calculates the equity of the second hand.
+     * Returns the equity of the second hand.
      * @return the equity expressed as a decimal number
      */
      double equity2();
